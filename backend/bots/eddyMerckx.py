f6e3732b0bb7d7b4dc4e95419919d993f307757b
gbxdl <daas.leer@gmail.com>
2026-05-03 15:34:44 +0200

    ok het moet wel vol

--- backend/bots/eddyMerckx.py
+++ backend/bots/eddyMerckx.py
@@ -1,5 +1,8 @@
-import random
+from tokenize import String
 from typing import TypedDict
+
+SPOTS_PER_TEAM: int = 8
+TONNETJE = 100_000
 
 # wordt alleen aangeroepen als:
 # - je team niet vol is
@@ -55,6 +58,98 @@ class BotResponse(TypedDict):
     amount: int | None # hoeveel je wil bieden, moet deelbaar zijn door een ton
     comment: str # leuk berichtje doe iedereen de groeten
 
+class theSuperComputer:
+
+    def __init__(self):
+        # initialize these as instance variables using 'self'
+        self.bestGcStat = 0
+        self.bestClimbStat = 0
+        self.bestSprintStat = 0
+        self.bestTtStat = 0
+        self.bestOneDayStat = 0
+        self.bestHillStat = 0
+        self.bestTotalStats = 0
+
+        self.inflationActive = 1
+
+    def updateBestDataSeen(self, rider_info: RiderInfo | None):
+        if rider_info is None:
+            return
+        rider_info['pointsPerSpeciality']
+        stats = rider_info.get('pointsPerSpeciality') or {}
+
+        self.bestGcStat = max(self.bestGcStat, stats.get('gc') or 0)
+        self.bestClimbStat = max(self.bestClimbStat, stats.get('climber') or 0)
+        self.bestSprintStat = max(self.bestSprintStat, stats.get('sprint') or 0)
+        self.bestTtStat = max(self.bestTtStat, stats.get('timeTrial') or 0)
+        self.bestOneDayStat= max(self.bestOneDayStat, stats.get('oneDayRaces') or 0)
+        self.bestHillStat = max(self.bestHillStat, stats.get('hills') or 0)
+        self.bestTotalStats = max(self.bestTotalStats, self.getTotalStats(stats))
+
+    def determineComment(self, rider_info: RiderInfo | None):
+        if rider_info is None:
+            return "... ik ging wel een beetje uit van PCS"
+        rider_info['pointsPerSpeciality']
+        stats = rider_info.get('pointsPerSpeciality') or {}
+        if (self.getTotalStats(stats) > self.bestTotalStats):
+            return "die alles kan"
+        if ((stats.get('gc') or 0) > self.bestGcStat):
+            return "voor het algemeen klassement"
+        if ((stats.get('climber') or 0) > self.bestClimbStat):
+            return "voor het klimmen"
+        if ((stats.get('sprint') or 0) > self.bestClimbStat):
+            return "voor het sprinten"
+        if ((stats.get('timeTrial') or 0) > self.bestClimbStat):
+            return "voor het tijdrijden"
+        if ((stats.get('oneDayRaces') or 0) > self.bestClimbStat):
+            return "voor etappes"
+        if ((stats.get('hills') or 0) > self.bestClimbStat):
+            return "voor de heuveltjes"
+        return "die niks kan"
+
+    def determineBid(self, rider_info: RiderInfo | None, highestBid, you: YouState) -> int | None :
+        if rider_info is None:
+            return
+        rider_info['pointsPerSpeciality']
+        stats = rider_info.get('pointsPerSpeciality') or {}
+        tonnetjeMeer = min(you.get('moneyLeft'), (highestBid or 0) + TONNETJE)
+        if (self.getTotalStats(stats) > self.bestTotalStats):
+            return tonnetjeMeer
+        if ((stats.get('gc') or 0) > self.bestGcStat):
+            return tonnetjeMeer
+        if ((stats.get('climber') or 0) > self.bestClimbStat):
+            return tonnetjeMeer
+        if ((stats.get('sprint') or 0) > self.bestClimbStat):
+            return tonnetjeMeer
+        if ((stats.get('timeTrial') or 0) > self.bestClimbStat):
+            return tonnetjeMeer
+        if ((stats.get('oneDayRaces') or 0) > self.bestClimbStat):
+            return tonnetjeMeer
+        if ((stats.get('hills') or 0) > self.bestClimbStat):
+            return tonnetjeMeer
+        return None
+
+    def getTotalStats(self, stats):
+        return (stats.get('gc') or 0) + (stats.get('climber') or 0) + (stats.get('sprint') or 0) + (stats.get('timeTrial') or 0) + (stats.get('oneDayRaces') or 0) + (stats.get('hills') or 0)
+
+    def computeSpotsLeft(self, you: YouState, others: list[OtherState]):
+        spotsLeft = SPOTS_PER_TEAM - len(you['riders'])
+        for other in others:
+            spotsLeft += SPOTS_PER_TEAM - len(other['riders'])
+        return spotsLeft
+
+    def applyInflation(self, multiple):
+        self.inflationActive = multiple
+        self.bestGcStat *= multiple
+        self.bestClimbStat *= multiple
+        self.bestSprintStat *= multiple
+        self.bestTtStat *= multiple
+        self.bestOneDayStat *= multiple
+        self.bestHillStat *= multiple
+        self.bestTotalStats *= multiple
+
+superComputer = theSuperComputer()
+
 def bot(
     rider: str, # naam zoals op https://www.procyclingstats.com/race/giro-d-italia/2026/startlist/alphabetical
     rider_bib: int, # nummer zoals op https://www.procyclingstats.com/race/giro-d-italia/2026/startlist/alphabetical (-1 als het niet bekend is)
@@ -67,12 +162,33 @@ def bot(
     previous_riders: list[str], # wie er al zijn geweest in alfabetische volgorde (exclusief huidige)
     rider_info: RiderInfo | None, # wat info van PCS, maar misschien ook niet
 ) -> BotResponse:
-    base = (highest_bid or 0)
-    random_amount = base + 100_000 * random.randint(1, 6)
+    if len(upcoming_riders) > 2 * len(previous_riders):
+        superComputer.updateBestDataSeen(rider_info)
+        return {
+        "amount": None,
+        "comment": "ik verzamel even alleen DATA: "
+        + str(superComputer.bestOneDayStat) + ", "
+        + str(superComputer.bestGcStat) + ", "
+        + str(superComputer.bestTtStat) + ", "
+        + str(superComputer.bestSprintStat) + ", "
+        + str(superComputer.bestClimbStat) + ", "
+        + str(superComputer.bestHillStat) + ", "
+        + str(superComputer.bestTotalStats)
+        }
 
-    do_bid = random.random() < 0.5
+    if (len(upcoming_riders) * 3 < superComputer.computeSpotsLeft(you, others)): #dus een derde moet nog gekocht worden
+        if superComputer.inflationActive > 0.9:
+            superComputer.applyInflation(0.9)
+
+    if (len(upcoming_riders) * 2 < superComputer.computeSpotsLeft(you, others)): #dus de helft moet nog gekocht worden
+        if superComputer.inflationActive > 0.5:
+            superComputer.applyInflation(0.5)
+
+
+    comment = "wow ja ik zocht nog iemand " + superComputer.determineComment(rider_info)
+    bot = superComputer.determineBid(rider_info, highest_bid, you)
 
     return {
-        "amount": min(you["moneyLeft"], random_amount) if do_bid else None,
-        "comment": "ok dit is echt mijn laatste bod" if do_bid else "deze hoef ik niet",
+        "amount": bot,
+        "comment": comment
     }
