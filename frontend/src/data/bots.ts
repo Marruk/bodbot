4986ac0483389bd3c9774b212fa82f7ddf88bce7
Mark Montvai <mark.montvai@gmail.com>
2026-07-16 19:40:26 +0200

    de tour wacht op niemand

--- frontend/src/data/bots.ts
+++ frontend/src/data/bots.ts
@@ -6,71 +6,65 @@ import wout from "../bots/dummies/wout"
 
 // Ok nu de echte
 import mark from "../bots/mark.ts"
-// import niels from "../bots/niels"
-// import daan from "../bots/daan";
-// import tom from "../bots/tom";
-// import lucas from "../bots/lucas"
-// import hannah from "../bots/hannah/hannah"
+import niels from "../bots/niels.ts"
+import daan from "../bots/daan";
+import tom from "../bots/tom.ts";
+import lucas from "../bots/lucas.ts"
 import joran from "../bots/joran/joran"
+// import hannah from "../bots/hannah/hannah"
 // import hendrik from "../bots/hendrik"
 
 import type { Bot } from "../models/auction.models"
 
 export const BOTS: { [key: string]: Bot } = {
-  jonas: {
-    key: "jonas",
-    owner: "Jonas",
-    name: "Hatsiekadee",
-    code: jonas,
-  },
-  tadej: {
-    key: "tadej",
-    owner: "Tadej",
-    name: "Hotseflots",
-    code: tadej,
-  },
-  wout: {
-    key: "wout",
-    owner: "Wout",
-    name: "Gezellig",
-    code: wout,
-  },
+  // jonas: {
+  //   key: "jonas",
+  //   owner: "Jonas",
+  //   name: "Hatsiekadee",
+  //   code: jonas,
+  // },
+  // tadej: {
+  //   key: "tadej",
+  //   owner: "Tadej",
+  //   name: "Hotseflots",
+  //   code: tadej,
+  // },
+  // wout: {
+  //   key: "wout",
+  //   owner: "Wout",
+  //   name: "Gezellig",
+  //   code: wout,
+  // },
   mark: {
     key: "mark",
     owner: "Mark",
     name: "Lekker Fietsen",
     code: mark,
   },
-  // 'hendrik': {
-  //   key: 'hendrik',
-  //   owner: 'De Betere Niels',
-  //   name: 'Lekker blijven trappen',
-  //   code: hendrik
-  // },
-  // 'tom': {
-  //   key: 'tom',
-  //   owner: 'Tom',
-  //   name: 'Lekker Fietsen',
-  //   code: tom
-  // },
-  // daan: {
-  //   key: "daan",
-  //   owner: "Daan",
-  //   name: "De ultieme veiler",
-  //   code: daan,
-  // },
-  // 'niels': {
-  //   key: 'niels',
-  //   owner: 'Niels',
-  //   name: 'De gezelligstebiedbot',
-  //   code: niels
-  // },
-  // 'lucas': {
-  //   key: 'lucas',
-  //   owner: 'Lucas',
-  //   name: 'Lucadance 100rpm',
-  //   code: lucas
-  // },
+  'tom': {
+    key: 'tom',
+    owner: 'Tom',
+    name: 'Lekker Fietsen',
+    code: tom
+  },
+  daan: {
+    key: "daan",
+    owner: "Daan",
+    name: "De ultieme veiler",
+    code: daan,
+  },
+  'niels': {
+    key: 'niels',
+    owner: 'Niels',
+    name: 'De gezelligstebiedbot',
+    code: niels
+  },
+  'lucas': {
+    key: 'lucas',
+    owner: 'Lucas',
+    name: 'Lucadance 100rpm',
+    code: lucas
+  },
   joran: {
     key: "joran",
     owner: "Joran",
@@ -82,5 +76,11 @@ export const BOTS: { [key: string]: Bot } = {
   //   owner: 'Hannah',
   //   name: 'Hoi',
   //   code: hannah
+  // },
+  // 'hendrik': {
+  //   key: 'hendrik',
+  //   owner: 'De Betere Niels',
+  //   name: 'Lekker blijven trappen',
+  //   code: hendrik
   // },
 }
